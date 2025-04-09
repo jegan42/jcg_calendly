25eadf191d2891c27e3b759aa43cba7d46c03251
jegan42 <aznchristophe@gmail.com>
2025-04-09 14:06:02 +0200

    🛠️ Add global.d.ts

---
+++ back/global.d.ts
@@ -0,0 +1,11 @@
+// global.d.ts
+declare module 'passport';
+declare module 'passport-google-oauth20';
+declare module 'express';
+declare module 'jsonwebtoken';
+declare module 'morgan';
+declare module 'cookie-parser';
+declare module 'cors';
+declare module 'csurf';
+declare module 'nodemailer';
+declare module 'escape-html';
