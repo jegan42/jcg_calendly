b71be0f73d15fc57bdee409b5ab54b7748880944
jegan42 <aznchristophe@gmail.com>
2025-04-16 20:08:21 +0200

    fix id by idgoogle

--- back/src/middleware/jwtAuth.ts
+++ back/src/middleware/jwtAuth.ts
@@ -31,7 +31,7 @@ export const requireJWTAuth = async (
         const { data: user, error } = await supabase
             .from("users")
             .select("*")
-            .eq("id", decoded.id)
+            .eq("id_google", decoded.id)
             .single();
 
         if (error || !user) {
