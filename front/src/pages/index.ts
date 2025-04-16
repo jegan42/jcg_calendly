95af418632a5b42448af74915e1fd2074b7b41ee
jegan42 <aznchristophe@gmail.com>
2025-04-16 14:33:45 +0200

    add pages/index

--- front/src/pages/index.ts
+++ front/src/pages/index.ts
@@ -1,22 +1,40 @@
 // src/pages/index.ts
+import Availability from "./Availability";
+import Calendar from "./Calendar";
 import CreateEvent from "./CreateEvent";
 import Dashboard from "./Dashboard";
 import EventDetail from "./EventDetail";
+import EventEdit from "./EventEdit";
 import EventForm from "./EventForm";
 import EventPage from "./EventPage";
 import Events from "./Events";
+import GuestResponse from "./GuestResponse";
 import Home from "./Home";
 import Login from "./Login";
+import Logout from "./Logout";
+import NotFound from "./404";
 import Profile from "./Profile";
+import PublicBooking from "./PublicBooking";
+import RecurringEvents from "./RecurringEvents";
+import Unauthorized from "./Unauthorized";
 
 export {
+    Availability,
+    Calendar,
     CreateEvent,
     Dashboard,
     EventDetail,
+    EventEdit,
     EventForm,
     EventPage,
     Events,
+    GuestResponse,
     Home,
     Login,
+    Logout,
+    NotFound,
     Profile,
+    PublicBooking,
+    RecurringEvents,
+    Unauthorized,
 };
