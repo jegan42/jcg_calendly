3431afd67b51f975a55ec6c245940110a73e0bba
jegan42 <aznchristophe@gmail.com>
2025-04-22 19:21:58 +0200

    chg add time

--- front/src/utils/funtions.ts
+++ front/src/utils/funtions.ts
@@ -2,11 +2,14 @@ export const getDefaultTimes = (date: string) => {
     if (!date) {
         return { defaultStart: "", defaultEnd: "" };
     }
+    const inputTime = new Date(date).toISOString().slice(11, 13);
+    let addNine = 0;
+    if (inputTime === "00") addNine = 9;
     const base = new Date(date);
-    const defaultStart = new Date(base.getTime() + 9 * 60 * 60 * 1000)
+    const defaultStart = new Date(base.getTime() + addNine * 60 * 60 * 1000)
         .toISOString()
         .slice(0, 16);
-    const defaultEnd = new Date(base.getTime() + 10 * 60 * 60 * 1000)
+    const defaultEnd = new Date(base.getTime() + (addNine + 1) * 60 * 60 * 1000)
         .toISOString()
         .slice(0, 16);
     return { defaultStart, defaultEnd };
